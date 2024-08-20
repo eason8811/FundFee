3bd3b1dbcc48194408421350f1202e25fc884013
eason8811 <3220563569@qq.com>
2024-08-20 10:55:09 +0800

    添加出场程序

---
+++ totalFundFee.py
@@ -0,0 +1,26 @@
+from BinanceAPI import BinanceAPI
+import time
+
+base_url_coin = 'https://dapi.binance.com'
+api_key = 'i7F6rx3Tcz6eJlSVzBc4dpV6qyszCiCOIpSz7gv9mdyq9UjVizrlu2kkmlvUIJSw'
+secret_key = 'mwU7KCworFZ17WIOqRuGaRmtwT3nnUDBhtg8HQf9CHFB7KVSxev0Rwym5mgfWjDx'
+
+coin_api = BinanceAPI(base_url_coin, api_key, secret_key)
+symbols = ['WIFUSD_PERP']
+
+for symbol in symbols:
+    body = {
+        'symbol': symbol,
+        'incomeType': 'FUNDING_FEE',
+        'startTime': int(time.mktime(time.strptime('2024-08-14 0:0:0', '%Y-%m-%d %H:%M:%S'))) * 1000,
+        'endTime': int(time.time()) * 1000,
+        'timestamp': int(time.time()) * 1000,
+    }
+    respond = coin_api.api('GET', '/dapi/v1/income', body)
+
+    fund_fee_list = []
+    for item in respond:
+        fund_fee_list.append(float(item['income']))
+
+    print(fund_fee_list)
+    print(f'总额为 {round(sum(fund_fee_list), 9)}')
